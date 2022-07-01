a21fdefae0278ec8550f8286688cabbbef38ecbc
Rupali Gaqndole <gajerupali81@gmail.com>
2022-07-01 14:25:12 +0530

    Initial commit

---
+++ src/org/school/Main.java
@@ -0,0 +1,53 @@
+package org.school;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        Teacher teacher1 = new Teacher(1,"teacher1",5000);
+        Teacher teacher2 = new Teacher(2,"teacher2",7000);
+        Teacher teacher3 = new Teacher(3,"teacher3",9000);
+
+        List<Teacher> teacherList = new ArrayList<>();
+        teacherList.add(teacher1);
+        teacherList.add(teacher2);
+        teacherList.add(teacher3);
+
+        Student student1 = new Student(1,"student1",4);
+        Student student2 = new Student(2,"student2",12);
+        Student student3 = new Student(3,"student3",5);
+        List<Student> studentList = new ArrayList<>();
+
+        studentList.add(student1);
+        studentList.add(student2);
+        studentList.add(student3);
+        
+        School college1 = new School(teacherList,studentList);
+
+        Teacher teacher4 = new Teacher(6,"teacher4", 1000);
+
+        college1.addTeacher(teacher4);
+
+
+        student1.payFees(25000);
+        student2.payFees(10000);
+        System.out.println("college1 has earned "+ college1.getTotalMoneyEarned());
+
+        System.out.println("------Making SCHOOL PAY SALARY----");
+        teacher1.receiveSalary(teacher1.getSalary());
+        System.out.println("college1 has spent for salary to " + teacher1.getName()
+        +" and now has " + college1.getTotalMoneyEarned());
+
+        teacher2.receiveSalary(teacher2.getSalary());
+        System.out.println("college1 has spent for salary to " + teacher2.getName()
+                +" and now has " + college1.getTotalMoneyEarned());
+
+
+        System.out.println(student2);
+
+        teacher3.receiveSalary(teacher3.getSalary());
+
+        System.out.println(teacher3);
+    }
+}
